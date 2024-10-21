179d2b8e42bbc956f35466bb6b89a0bab972037a
[Sean Albertyn] <sean.albertyn04@gmail.com>
2024-10-22 09:38:26 +1300

    Created the database
    Created the database and made migrations

--- musicproject/settings.py
+++ musicproject/settings.py
@@ -77,10 +77,9 @@ WSGI_APPLICATION = 'musicproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3', 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -99,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
