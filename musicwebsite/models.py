7ef17bacdc9b71938ac4f263d25d8f5b32a356ed
[Sean Albertyn] <sean.albertyn04@gmail.com>
2024-10-22 11:42:13 +1300

    Created the models
    Created the Chord, Song and SongChord models the the models.py file along with making the migrations

--- musicwebsite/models.py
+++ musicwebsite/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+# This is the defined 'Song' model 
+class Song(models.Model):
+    
+    # This is the Forgeign Key to User aspect of the model
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='songs')
+
+    # These are the fields related to the 'Song' model
+    title = models.CharField(max_length=50)
+
+    # These are Timestaps for when the song was created or updated
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return super().__str__()
+
+# This is the defined 'Chord' model
+class Chord(models.Model):
+
+    # This is the Forgeign Key to user aspect of the model
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chords')
+
+    # These are the fields related to the 'Chord' model
+    rootname = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    iscustom = models.BooleanField()
+    fingerposition1 = models.IntegerField()
+    fingerposition2 = models.IntegerField() 
+    fingerposition3 = models.IntegerField() 
+    fingerposition4 = models.IntegerField() 
+    fingerposition5 = models.IntegerField() 
+    fingerposition6 = models.IntegerField()  
+
+    def __str__(self):
+        return super().__str__()
+
+
+# This is the defined 'SongChord' model
+class SongChord(models.Model):
+
+    # These are the Forgeign Keys to the Song and Chord Models
+    song = models.ForeignKey(Song, on_delete=models.CASCADE, related_name='songchords')
+    chord = models.ForeignKey(Chord, on_delete=models.CASCADE, related_name='songchords')
+
+    # These are the fields related to the 'SongChord' model
+    position = models.IntegerField()
+    length = models.IntegerField()
+
+    def __str__(self):
+        return super().__str__()
+    
