2a9a9630e6800f6372957a4430c5e283f266df7b
Carol.D <caroldo101@gmail.com>
2024-10-24 14:12:11 +1300

    Added Bootstrap
    Added bootstrap theme "United" from Bootswatch.

--- musicwebsite/views.py
+++ musicwebsite/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+# The views.
 def index(request):
     context = {
         "context_name" : "context data"
@@ -9,3 +10,5 @@ def index(request):
     return render(request, 'musicwebsite/index.html', context)
 
 
+# def home(request):
+#     return HttpResponse ('<h1> testing <h1/>')
