41472ccbcbe07095594f64b0b11529f844e1d242
Bailey Thorp <bailey.thorp@techtorium.ac.nz>
2024-10-31 16:30:26 +1300

    functioning chord selector. Still need to highlight selected chord type / note. and account for muted strings

--- musicwebsite/views.py
+++ musicwebsite/views.py
@@ -3,12 +3,30 @@ from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from .models import Chord
 from musicwebsite.forms import ChordVerificationForm
+from django.core import serializers
+from django.forms.models import model_to_dict
+
+from json import dumps
+from json import dump
 
 
 # The views.
 def index(request):
+    
+    chordList = list(Chord.objects.all())
+    #frets = [chord.frets for chord in chords]
+    
+    dictList = []
+    for x in chordList:
+        dictList.append(model_to_dict(x))    
+    
+    dataJSON = dumps(dictList)
+    
+    #with open('data.json', 'w') as f:
+    #    dump(dataJSON, f)
+    
     context = {
-        "context_name" : "context data"
+        'data' : dataJSON,
     }
     return render(request, 'musicwebsite/index.html', context)
 
